5a5608304afac8f46165698b6e6aba90b5d5f16b
seyoung <skview1201@gmail.com>
2020-05-22 22:17:30 +0900

    유니티 게임인터페이스설계
    메인화면 - 스테이지 연결
    UI개발중

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menu_set;
+    private int stage_number;
+
+    void Start()
+    {
+        stage_number = 0;
+        menu_set.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) 
+        if (menu_set.activeSelf)
+            menu_set.SetActive(false);
+        else
+            menu_set.SetActive(true);
+    }
+
+    public void PressStart()
+    {
+        stage_number += 1;
+        SceneManager.LoadScene("stage"+stage_number.ToString());
+    }
+    public void PressExit()
+    {
+        //유니티 에디터에서 종료
+        UnityEditor.EditorApplication.isPlaying = false;
+        //게임에서 종료
+        //Application.Quit();
+    }
+    public void PressBackTitle()
+    {
+        menu_set.SetActive(false);
+        stage_number = 0;
+        SceneManager.LoadScene("Title");
+    }
+}
