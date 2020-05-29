940cb518d82a8d9f4f0cb46ca1f8818cb0b6fbdf
seyoung <skview1201@gmail.com>
2020-05-29 18:42:58 +0900

    스킬 드래그함수 타일맵 구현

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Actions : MonoBehaviour
+{
+    private GameObject player;
+    private Vector2 player_pos;
+    private GameObject action1;
+    private GameObject action2;
+    private GameObject action3;
+    private GameObject action4; 
+    private GameObject action5;
+    private GameObject action6;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        player_pos = player.transform.position;
+        action1 = GameObject.Find("Action1");
+        action2 = GameObject.Find("Action2");
+        action3 = GameObject.Find("Action3");
+        action4 = GameObject.Find("Action4");
+        action5 = GameObject.Find("Action5");
+        action6 = GameObject.Find("Action6");
+    }
+
+    void Update()
+    {
+        action1.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(1.0f, 1.2f));
+        action2.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(1.5f, 0.0f));
+        action3.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(1.0f, -1.2f));
+        action4.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(-1.0f, -1.2f));
+        action5.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(-1.5f, 0.0f));
+        action6.transform.position = Camera.main.WorldToScreenPoint(player_pos + new Vector2(-1.0f, 1.2f));
+    }
+
+    private void OnTriggerEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Caster")
+        { 
+            this.transform.localScale = new Vector2(2.0f, 2.0f);
+            Debug.Log("충돌");
+        }
+    }
+
+    private void OnTriggerExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Caster")
+        {
+            this.transform.localScale = new Vector2(0.5f, 0.5f);
+            Debug.Log("충돌 끝");
+        }
+    }
+}
---
+++ Assets/Scripts/Caster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Caster : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private GameObject player;
+    private Vector2 player_pos;
+    private Vector2 default_pos;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        player_pos = player.transform.position;
+        this.transform.position = Camera.main.WorldToScreenPoint(player_pos);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        default_pos = this.transform.position;
+    } 
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 mouse_pos = Input.mousePosition;
+        this.transform.position = mouse_pos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.position = default_pos;
+    }
+}
