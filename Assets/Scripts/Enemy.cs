507ab4cd3f1934243c4984c0daf6b81eca3dbebb
seyoung <skview1201@gmail.com>
2020-06-04 17:45:51 +0900

    스크립트 주석 추가
    이해안되는 게 있으면 카톡해주세요

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//적의 정보가 있는 스크립트
 public class Enemy : MonoBehaviour
 {
-    private GameObject actions;
     private int health_point, action_point, attack, defence;
+    //C#에서는 getter와 setter를 private 변수의 앞글자를 대문자로 바꾼 public 변수를 만들어 사용한다고 함
     public int Health_Point
     { set; get; }
     public int Action_Point
@@ -16,15 +17,16 @@ public class Enemy : MonoBehaviour
     { set; get; }
 
     void Start()
-    {
+    {   
         if (this.gameObject.tag == "Enemy")
         {
-            health_point = 10;
-            action_point = 4;
-            attack = 2;
-            defence = 2;
+            health_point = 30;
+            action_point = 5;
+            attack = 5;
+            defence = 5;
         }
 
+        //차후 스크린 좌표 위치에 맞게 수정 예정 - 이세영
         this.gameObject.transform.position = (this.gameObject.transform.position);
     }
 
--- Assets/Scripts/PlayerBattle.cs
+++ Assets/Scripts/PlayerBattle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Battle 스테이지의 플레이어에 관한 스크립트
 public class PlayerBattle : MonoBehaviour
 {
     private GameObject target;
@@ -10,6 +11,7 @@ public class PlayerBattle : MonoBehaviour
     static public bool is_battled;
 
     private int health_point, action_point, attack, defence;
+    //C#에서는 getter와 setter를 private 변수의 앞글자를 대문자로 바꾼 public 변수를 만들어 사용한다고 함
     public int Health_Point
     { set; get; }
     public int Action_Point
@@ -21,13 +23,14 @@ public class PlayerBattle : MonoBehaviour
 
 
     void Start()
-    {
+    {   
+        //Battle 스테이지 돌입 시 
         is_battled = true;
         actions = GameObject.Find("Actions");
         actions.SetActive(false);
         if (this.gameObject.tag == "Player")
         {
-            health_point = 10;
+            health_point = 100;
             action_point = 5;
             attack = 5;
             defence = 5;
@@ -37,16 +40,19 @@ public class PlayerBattle : MonoBehaviour
     void FixedUpdate()
     {
         CastRay();
+        //마우스가 플레이어 위에 있다면 액션 창이 활성화
         if (target == this.gameObject)
-        {
+        {    
             actions.SetActive(true);
         }
 
         CastRay();
+        //마우스 버튼이 액션 이미지 위에서 떼어졌다면 == 액션을 취했으면 비활성화
         if (target != this.gameObject && !Caster.is_active)
                 actions.SetActive(false);
     }
 
+    //광선을 쏴서 마우스와 충돌한 오브젝트를 판별하는 함수
     void CastRay()
     {
         target = null;
