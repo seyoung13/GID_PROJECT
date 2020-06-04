5c207a5d9ca20e33756ef25f3fdd927cd5b586e7
Hangbul <wns0685@gmail.com>
2020-06-04 20:01:21 +0900

    스프라이트, 이미지 추가_임 준

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//플레이어를 따라가는 카메라를 만들기 위한 스크립트
 public class CameraMove : MonoBehaviour
 {
     public Transform target;
     private Transform main_camera;
 
     void Start()
-    {
-        main_camera = GetComponent<Transform>();
-        
+    {   
+        //위치 정보를 가져옴
+        main_camera = GetComponent<Transform>();    
     }
 
     void LateUpdate()
-    {
+    {   
+        //2D이지만 카메라는 z축이 있으므로 유지해야 함
         main_camera.position = new Vector3(target.position.x + 6.5f, target.position.y + 0.5f, main_camera.transform.position.z);
     }
 }
