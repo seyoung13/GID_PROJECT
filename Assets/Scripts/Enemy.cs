8a986d03fc160db34016fbf93c9461aa39b9c05b
Hangbul <wns0685@gmail.com>
2020-06-25 08:14:41 +0900

    6/25/spriteadd

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,8 +12,11 @@ public class Enemy : MonoBehaviour
     private StateMachine state;
     private Animator EnemyAnimator;
     public bool ActionEnd;
+   
+
+    private PathFinding tilemap;
     //public List<string> EnemyAi = new List<string>() { "attack", "move", "idle" };
-     //C#에서는 getter와 setter를 private 변수의 앞글자를 대문자로 바꾼 public 변수를 만들어 사용한다고 함
+    //C#에서는 getter와 setter를 private 변수의 앞글자를 대문자로 바꾼 public 변수를 만들어 사용한다고 함
     public int Health_Point
     { set { health_point = value; } get { return health_point; } }
     public int Action_Point
@@ -29,6 +32,8 @@ public class Enemy : MonoBehaviour
     public StateMachine State
     { set { state = value; } get { return state; } }
 
+    public Vector2 position;
+
     void Start()
     {
         EnemyAnimator = GetComponent<Animator>();
@@ -43,62 +48,63 @@ public class Enemy : MonoBehaviour
         state = StateMachine.END;
         Debug.Log(EnemyAnimator);
     }
-
-    /*
     public void ai()
     {
-        if (TurnButton.EnemyTurn)
+        position = transform.position;
+       
+        for (int i = 0; i < 3; ++i)
         {
-            for (int i = 0; i < 3; ++i)
+            EnemyAi rand = (EnemyAi)Random.Range(0, 2);
+            if (action_point > 0)
             {
-                EnemyAi rand = (EnemyAi)Random.Range(0,2);
-                if (action_point > 0)
+                if (rand == EnemyAi.attack)
                 {
-                    Debug.Log(rand);
-                    Debug.Log("액션포인트");
-                    if (rand==EnemyAi.attack)
-                    {
-                        Debug.Log("몬스터 공격");
-                        EnemyAnimator.SetTrigger("is_attack");
-                        TurnButton.EnemyTurn = false;
-                    }
-                    else if (rand == EnemyAi.move)
-                    {
-                        Debug.Log("몬스터 이동");
-                        EnemyMove move = (EnemyMove)Random.Range(0, 3);
-                        if (move == EnemyMove.up)
-                        {
-                         
-                        }
-                        else if (move == EnemyMove.down)
-                        {
+                    Debug.Log("몬스터 공격");
+                    EnemyAnimator.SetTrigger("is_attack");
 
-                        }
-                        else if (move == EnemyMove.left)
-                        {
-
-                        }
-                        else if (move == EnemyMove.right)
-                        {
-
-                        }
-                        
-                        TurnButton.EnemyTurn = false;
-                    }
-                    else if (rand == EnemyAi.idle)
-                    {
-                        Debug.Log("몬스터 대기");
-                        EnemyAnimator.SetBool("is_die", true);
-                        TurnButton.EnemyTurn = false;
-                    }
                 }
-                Debug.Log("액션포인트 끝");
+                else if (rand == EnemyAi.move)
+                {
+                    Debug.Log("몬스터 이동");
+                    EnemyMove move = (EnemyMove)Random.Range(0, 4);
+                    if (move == EnemyMove.up)
+                    {
+                        if(position.y<0)
+                            position.y +=0.8f;
+                    }
+                    else if (move == EnemyMove.down)
+                    {
+                        if(position.y>-3)
+                            position.y -= 0.8f;
+                    }
+                    else if (move == EnemyMove.left)
+                    {
+                        if(position.x>-4.34f)
+                            position.x -= 2.5f;
+                    }
+                    else if (move == EnemyMove.right)
+                    {
+                        if(position.x<6.5f)
+                            position.x += 2.5f;
+                    }
+
+                    transform.position = position;
+                }
+                else if (rand == EnemyAi.idle)
+                {
+                    Debug.Log("몬스터 대기");
+                    EnemyAnimator.SetBool("is_die", true);
+
+                }
             }
-            
+            Debug.Log("액션포인트 끝");
+        }
+     
+      
             //action_point -= 1;
             //Debug.Log("몬스터 끝");
             //Debug.Log(action_point);
-        }
+           
     }
-    */
+
 }
